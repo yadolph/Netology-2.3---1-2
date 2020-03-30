2231143883bfb1708ec721edb77d82ff503f096a
yadolph <oiv@live.ru>
2020-03-30 17:18:27 +0000

    1. Программа больше не принимает отрицательные числа 2. Блоки assert вынесены из блока try/except, убрана вложенность блоков try/except

--- main.py
+++ main.py
@@ -2,19 +2,21 @@
 
 def polishify(poland):
   sym_list = poland.split(' ')
+  
+  assert len(sym_list) == 3, "Неверное число операторов"
+  assert sym_list[0] in ['+','-','*','/'] , "Данная операция не поддерживается"
+  assert int(sym_list[1]) >=0 and int(sym_list[2]) >=0, "Введено отрицательное число"
+  
+  normal_line = (sym_list[1] + sym_list[0] + sym_list[2])
   try:
-    assert sym_list[0] in ['+','-','*','/'] , "Данная операция не поддерживается"
-    assert len(sym_list) == 3, "Вы ввели неверное количество символов или не разделили их пробелом"
-    normal_line = (sym_list[1] + sym_list[0] + sym_list[2])
-    try:
-      result = eval(normal_line)
-      print(result)
-    except ZeroDivisionError:
-      print('Вы попытались разделить на ноль. Ничего не вышло.')
-    except NameError:
-      print('Проверьте ваши числа. Возможно это совсем не числа')
-    except Exception:
-      print('Что-то пошло не так')
+    result = eval(normal_line)
+    print(result)
+  except ZeroDivisionError:
+    print('Вы попытались разделить на ноль. Ничего не вышло.')
+  except NameError:
+    print('Проверьте ваши числа. Возможно это совсем не числа')
+  except Exception:
+    print('Что-то пошло не так')
 
   except Exception as e:
     print(e)
